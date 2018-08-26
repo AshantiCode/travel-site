11a302412a3ecdae890fb411e953e6265b2d5d58
Ashanti <ashanti.haring@gmail.com>
2018-08-26 17:59:45 +0200

    Completed the reveal-on-scroll behavour

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    /*this.offsetPercentage = offset;*/
+    this.hideInitially();
+    this.createWaypoints(offset); //hier offset in den klammern statt der oben auskommentiertein Linie//
+    
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints(offset) {
+    /*var that = this;*/
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: offset
+      });
+    })
+  }
+}
+
+export default RevealOnScroll;
